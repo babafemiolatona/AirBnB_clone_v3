2f0ef60088f13455af170e713c15e5002f968653
babafemiolatona <femiolatona@gmail.com>
2023-10-30 13:44:33 +0100

    Fixed errors

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,9 +3,10 @@
 Creates a route /status on the object app_views that returns a JSON status
 """
 from api.v1.views import app_views
+from flask import jsonify
 
 
-@app_views.route('/status', strict_slashes=False)
+@app_views.route('/status')
 def status():
     """Returns a JSON status"""
-    return {"status": "OK"}
+    return jsonify ({"status": "OK"})
