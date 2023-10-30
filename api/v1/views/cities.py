95abe14e747f61c185ce16a04fff5a4a4547eb83
babafemiolatona <femiolatona@gmail.com>
2023-10-30 22:38:22 +0100

    Refactored code

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -66,7 +66,7 @@ def update_city(city_id):
     city_json = request.get_json()
     if not city_json:
         abort(400, description="Not a JSON")
-    city.name = city_json.get('name', city.name)
+    city.name = city_json('name', city.name)
     # for k, v in city_json.items():
     #     if k not in ['id', 'created_at', 'updated_at']:
     #         setattr(city, k, v)
